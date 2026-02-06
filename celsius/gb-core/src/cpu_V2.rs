841ec34a95c4ceb5a7bd3856a90a3544c24e1a49
Bridger Hall <bridgerrhall@gmail.com>
2026-02-06 13:00:51 -0700

    started outlining main in esp32 and cpu.rs

---
+++ celsius/gb-core/src/cpu_V2.rs
@@ -0,0 +1,49 @@
+struct Cu {
+
+}
+
+impl Cu {
+    pub new() ->Self{
+
+    }
+}
+
+struct Rf {
+
+}
+
+impl Rf {
+    pub new() ->Self{
+
+    }
+}
+
+struct Alu {
+
+}
+
+impl Alu {
+    pub new() ->Self{
+
+    }
+}
+
+struct Idu {
+
+}
+
+impl Idu {
+    pub new() ->Self{
+
+    }
+}
+
+struct Cpu {
+
+}
+
+impl Cpu {
+    pub fn new() ->Self{
+
+    }
+}
--- celsius/gb-esp32-s3/src/main.rs
+++ celsius/gb-esp32-s3/src/main.rs
@@ -5,14 +5,33 @@ use esp_backtrace as _;
 use esp_hal::main; // This provides the #[main] attribute
 use esp_println::println;
 
-#[main] // Replaces #[entry]
-fn main() -> ! {
-    // New way to initialize for v0.23+
-    let _peripherals = esp_hal::init(esp_hal::Config::default());
+// #[main] // Replaces #[entry]
+// fn main() -> ! {
+//     // New way to initialize for v0.23+
+//     let _peripherals = esp_hal::init(esp_hal::Config::default());
     
-    println!("Celsius Emulator: ESP32-S3 Target Online");
+//     println!("Celsius Emulator: ESP32-S3 Target Online");
 
-    loop {
-        // Emulator logic will go here
-    }
+//     loop {
+//         // Emulator logic will go here
+//     }
+// }
+
+
+let frame_ready: bool //core 1 needs to wait if display hasn't finished drawing last frame
+
+
+//need to instantiate all things i.e. catridge bus cpu
+
+//core 1: cpu/ppu
+loop {
+    //read instructions
+    cpu.tick(&mut bus); //cpu owns bus.  //need to keep track of cycles and pass to ppu
+    //check video ram update scanlines.  //so it can keep up with cpu !!!!must be in sync!!!
+    ppu.tick(&mut bus);
+}
+
+//core 0: everything else
+loop {
+    //do fancy stuff here
 }
