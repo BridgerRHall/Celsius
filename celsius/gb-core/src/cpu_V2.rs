c841e07dbb87318637b56009bab572eab6160e57
Bridger Hall <bridgerrhall@gmail.com>
2026-02-21 12:55:12 -0700

    started alu

--- celsius/gb-core/src/cpu_V2.rs
+++ celsius/gb-core/src/cpu_V2.rs
@@ -7,7 +7,6 @@ impl Cu {
 
     }
 }
-
 struct Rf {
 
 }
@@ -17,17 +16,6 @@ impl Rf {
 
     }
 }
-
-struct Alu {
-
-}
-
-impl Alu {
-    pub new() ->Self{
-
-    }
-}
-
 struct Idu {
 
 }
@@ -37,6 +25,8 @@ impl Idu {
 
     }
 }
+
+
 
 struct Cpu {
 
@@ -58,10 +48,9 @@ impl Cpu {
         stopped: bool = false;
         halted: bool = false;
 
-
     }
 
-    fn step(&self, &mut bus) -> u32 {
+    fn step(&mut self, bus: &mut Bus) -> u32 {
 
         if stopped {
             return 4;
@@ -122,6 +111,33 @@ impl Cpu {
                 }
 
             }
+            0x80..=0xBF => {
+                
+                let src_id = opcode & 0x7;
+                let op_id = (opdcode >> 3) & 0x07;
+                let value = self.get_register_val(&mut self, bus, src_id);
+
+                //match op_id to determine operation
+                match op_id { //translate u8 into int to match easily
+                    0 => self.alu(&mut self, value, 0x00);
+                    1 => self.alu(&mut self, value, 0x01);
+                    2 => sub;
+                    3 => sbc;
+                    4 => and;
+                    5 => xor;
+                    6 => or;
+                    7 => cp;
+
+                }
+
+                //return ticks
+                if src_id == 6 || dest_id == 6 {
+                    8
+                } else {
+                    4
+                }
+
+            }
             0x3E => { //LD A, n
                 self.a = bus.read(self.pc);
                 self.pc +=1;
@@ -149,4 +165,76 @@ impl Cpu {
         }
     }
 
+
+    fn get_register_val(&self, bus: &mut Bus, id: u8) -> u8 {
+        match id {
+            0 => self.b,
+            1 => self.c,
+            2 => self.d,
+            3 => self.e,
+            4 => self.h,
+            5 => self.l,
+            6 => bus.read(self.get_hl()),
+            7 => self.a,
+            _ => unreachable!(),
+        }
+    }
+
+
+    fn alu(&mut self, value: u8, arithmetic: u8) -> u8{
+        match arithmetic {
+            0x00 => { //add
+                let a = self.a;
+                let sum = (a as u16) + (value as u16);
+                let result = sum as u8;
+
+                //flags
+                let z = if result == 0 { 0x80 } else {0};
+                let n = 0x00;
+                let h = if (a & 0x0F) + (value & 0x0F) > 0x0F { 0x020 } else { 0 };
+                let c = if sum > 0xFF { 0x10 } else { 0 };
+
+                self.f = z | n | h | c;
+                self.a = result;
+
+            },
+            0x01 => { //adc
+                let carry_bit = (self.f >> 4 & 0x01);
+                let a = self.a;
+
+                let sum = (a as u16) + (value as u16) + (carry_bit as u16);
+                let result = sum as u8;
+
+                //z
+                let z = if result == 0 { 0x80 } else { 0 };
+                //sub
+                let n = 0x00;
+                //half carry
+                let h = if (((a & 0x0F) + (value & 0x0F) + carry_bit) > 0x0F) { 0x20 } else{ 0 };
+                //carry 
+                let c = if sum > 0xFF { 0x10 } else { 0 };
+
+                self.f = z | n | h | c;
+                self.a = result;
+            },
+            0x02 => {
+
+            },
+            0x03 => {
+
+            },
+            0x04 => {
+
+            },
+            0x05 => {
+
+            },
+            0x06 => {
+
+            },
+            0x07 => {
+
+            },
+        }
+    }
 }
